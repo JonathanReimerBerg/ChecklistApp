cdad4f41263a109b99a2bc97edf8bcc21c2190a9
Jonathan Reimer-Berg <jonathanreimerberg@gmail.com>
2022-03-24 17:18:11 -0400

    Adds sorting for checked items and date created, stores sort method, and applies sort when opening list

--- src/app/view-list/view-list.page.ts
+++ src/app/view-list/view-list.page.ts
@@ -25,6 +25,8 @@ export class ViewListPage implements OnInit {
     const id = this.activatedRoute.snapshot.paramMap.get('id');
     this.listID = parseInt(id, 10);
     this.list = this.checklistApiService.getCurrentList(this.listID);
+    this.sortType = this.list.sorting_method;
+    this.desc = !this.list.sorting_reversed
   }
 
   getListItems(): ListItem[] {
@@ -37,16 +39,16 @@ export class ViewListPage implements OnInit {
       header: 'Sorting Method',
       buttons: [{
         text: 'Alphabetical',
-        handler: (data) => {this.sortType = 'alphabetical', this.desc = !this.desc}
+        handler: (data) => {this.checklistApiService.modifyList(this.listID, null, null, "alphabetical", (this.list.sorting_method === "alphabetical")), this.sortType = 'alphabetical', this.desc = !this.list.sorting_reversed}
       }, {
         text: 'Completed',
-        handler: (data) => {this.sortType = 'completed', this.desc = !this.desc}
+        handler: (data) => {this.checklistApiService.modifyList(this.listID, null, null, "completed", (this.list.sorting_method === "completed")), this.sortType = 'completed', this.desc = !this.list.sorting_reversed}
       }, {
         text: 'Do By Date',
-        handler: (data) => {this.sortType = 'doByDate', this.desc = !this.desc}
+        handler: (data) => {this.checklistApiService.modifyList(this.listID, null, null, "doByDate", (this.list.sorting_method === "doByDate")), this.sortType = 'doByDate', this.desc = !this.list.sorting_reversed}
       }, {
         text: 'Date Created',
-        handler: (data) => {this.sortType = 'dateCreated', this.desc = !this.desc}
+        handler: (data) => {this.checklistApiService.modifyList(this.listID, null, null, "dateCreated", (this.list.sorting_method === "dateCreated")), this.sortType = 'dateCreated', this.desc = !this.list.sorting_reversed}
       }]
     });
     (await alert).present()
