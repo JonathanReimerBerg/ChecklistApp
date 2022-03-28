831a749c7c5ab10bd59a99d8548d9de727f8128f
cbrigug <cbrigug@gmail.com>
2022-03-28 17:37:47 -0400

    add toasted upon list delete

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { AlertController } from '@ionic/angular';
+import { AlertController, ToastController } from '@ionic/angular';
 import { appInitialize } from '@ionic/angular/app-initialize';
 import { ChecklistApiService, List, ListItem } from './../services/checklist-api.service';
 
@@ -12,9 +12,21 @@ export class ListComponent implements OnInit {
   @Input() list: List;
   private listID: number;
 
-  constructor(private checklistApiService: ChecklistApiService,public alertController: AlertController) { }
+  constructor(
+    private checklistApiService: ChecklistApiService,
+    public alertController: AlertController,
+    public toastController: ToastController
+    ) { }
 
   ngOnInit() {}
+
+  async presentToast(message: string) {
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 2000
+    });
+    toast.present();
+  }
 
   removeList(item){
     this.alertController.create({
@@ -22,7 +34,10 @@ export class ListComponent implements OnInit {
       message: 'Are you sure you want to delete the list?',
       buttons: [
         {text: 'Cancel', handler: (data: any) => {console.log('Canceled', this.list)}},
-        {text: 'Delete', handler: (data: any) => {this.checklistApiService.removeList(this.list);}}
+        {text: 'Delete', handler: (data: any) => {
+          this.checklistApiService.removeList(this.list);
+          this.presentToast("List deleted.");
+        }}
       ]
     }).then(res => {res.present()});
   }
